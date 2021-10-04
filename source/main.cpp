ceb2149c8161396c88378fbff20fdf0309010388
tkdieneb1999 <tkideneb44@gmail.com>
2021-10-04 14:58:31 +0200

    added GLFW and ImGui

--- source/main.cpp
+++ source/main.cpp
@@ -1,7 +1,58 @@
 #include <iostream>
+#include "GLFW/glfw3.h"
+
+#include "imgui.h"
+#include "backends/imgui_impl_glfw.h"
+#include "backends/imgui_impl_opengl3.h"
 
 int main()
 {
-	std::cout << "Test" << std::endl;
-	std::cin.get();
+	if (!glfwInit())
+	{
+		std::cout << "GLFW could not be initialized!" << std::endl;
+	}
+
+	GLFWwindow* window = glfwCreateWindow(1920, 1080, "Game Asset Pipeline Program", nullptr, nullptr);
+
+	if (!window)
+	{
+		std::cout << "Window Creation failed!" << std::endl;
+	}
+
+	glfwMakeContextCurrent(window);
+
+	IMGUI_CHECKVERSION();
+	ImGui::CreateContext();
+	ImGui::StyleColorsDark();
+
+	ImGui_ImplGlfw_InitForOpenGL(window, true);
+	const char* glslVersion = "#version 130";
+	ImGui_ImplOpenGL3_Init(glslVersion);
+
+	while (!glfwWindowShouldClose(window))
+	{
+		glfwPollEvents();
+
+		ImGui_ImplOpenGL3_NewFrame();
+		ImGui_ImplGlfw_NewFrame();
+		ImGui::NewFrame();
+
+		ImGui::ShowDemoWindow();
+
+		ImGui::Render();
+		int display_w, display_h;
+		glfwGetFramebufferSize(window, &display_w, &display_h);
+		glViewport(0, 0, display_w, display_h);
+		glClearColor(0, 0, 0, 1);
+		glClear(GL_COLOR_BUFFER_BIT);
+		ImGui_ImplOpenGL3_RenderDrawData(ImGui::GetDrawData());
+
+		glfwSwapBuffers(window);
+	}
+	ImGui_ImplOpenGL3_Shutdown();
+	ImGui_ImplGlfw_Shutdown();
+	ImGui::DestroyContext();
+
+	glfwDestroyWindow(window);
+	glfwTerminate();
 }
